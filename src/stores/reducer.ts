0402e11eb2ee05dc3f9d497edb2952cfcdf4ad27
taquy <taquy.se@gmail.com>
2021-02-09 18:24:34 +0700

    add reducer

--- src/stores/reducer.ts
+++ src/stores/reducer.ts
@@ -1,7 +1,7 @@
 
 
 import { combineReducers } from 'redux';
-import { UserReducer } from './user/user.reducer';
+import UserReducer from './user/user.reducer';
 
 export const rootReducer = combineReducers({
   users: UserReducer,
--- src/stores/user/user.reducer.ts
+++ src/stores/user/user.reducer.ts
@@ -1,5 +1,25 @@
 
+import { User } from '../../components/User/user';
+import { Action } from '../state';
+import { UserActions } from './user.action';
 
-export const UserReducer = {
-  
+interface StateInterface {
+  users: User[]
+}
+
+const initialState = {
+  users: [],
+}
+
+export default function UserReducer(state: StateInterface = initialState, action: Action) {
+  switch (action.type) {
+    case UserActions.GET_USERS_SUCCESS: {
+      return {
+        ...state,
+        users: action.payload
+      }
+    }
+    default:
+      return state
+  }
 }
