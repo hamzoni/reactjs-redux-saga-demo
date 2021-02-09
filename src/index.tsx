d12a073e88dacaefa38ab601cde657c243bee6ce
taquy <taquy.se@gmail.com>
2021-02-09 18:31:13 +0700

    register store

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,20 @@ import reportWebVitals from './reportWebVitals';
 
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import { Provider } from 'react-redux';
+import { rootReducer } from './stores/reducer';
+
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  rootReducer,
+  applyMiddleware(sagaMiddleware)
+)
 
 ReactDOM.render(
   <React.StrictMode>
+     <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
