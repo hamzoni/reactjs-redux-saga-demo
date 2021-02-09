c6921a9dfd2d853488416111c4c62cc549703d5e
taquy <taquy.se@gmail.com>
2021-02-09 18:58:21 +0700

    connecting component with action

--- src/components/User/user-table.tsx
+++ src/components/User/user-table.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Table } from "evergreen-ui";
-
+import { getUsers } from "../../stores/user/user.action";
+import connect from './user-table';
 function UserTable() {
   const [users, setUsers] = useState([
     {
@@ -18,9 +19,7 @@ function UserTable() {
       </Table.Head>
       <Table.Body height={240}>
         {users.map((user) => (
-          <Table.Row
-            key={user.id}
-          >
+          <Table.Row key={user.id}>
             <Table.TextCell>{user.name}</Table.TextCell>
             <Table.TextCell isNumber>{user.age}</Table.TextCell>
           </Table.Row>
@@ -30,4 +29,11 @@ function UserTable() {
   );
 }
 
-export default UserTable;
+const mapDispatchToProps = {
+  getUsers,
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(UserTable)
