bc5db982ce3fcef4242519f12e0d777e5b13e711
taquy <taquy.se@gmail.com>
2021-02-09 21:27:58 +0700

    add paging

--- src/components/User/user-table.tsx
+++ src/components/User/user-table.tsx
@@ -31,33 +31,75 @@ const UserTable: React.FC<UserTableProps> = (props) => {
     setUsers(props.users);
   }, [props.users]);
 
+  const setPage = (page: number) => {
+    setPageRequest({
+      ...pageRequest,
+      page
+    });
+  }
+
+  const PagingComponent = () => {
+    return (
+      <nav className="mt-3 d-flex justify-content-center">
+        <ul className="pagination mx-auto">
+          {pageRequest.page > 1 && (
+            <li className="page-item">
+              <span className="page-link">
+                <span aria-hidden="true">&laquo;</span>
+              </span>
+            </li>
+          )}
+          {pageRequest.page > 1 && (
+            <li className="page-item" onClick={() => setPage(pageRequest.page - 1)}>
+              <span className="page-link">{pageRequest.page - 1}</span>
+            </li>
+          )}
+          <li className="page-item active">
+            <span className="page-link">{pageRequest.page}</span>
+          </li>
+          <li className="page-item" onClick={() => setPage(pageRequest.page + 1)}>
+            <span className="page-link">{pageRequest.page + 1}</span>
+          </li>
+          <li className="page-item" onClick={() => setPage(pageRequest.page + 1)}>
+            <span className="page-link">
+              <span aria-hidden="true">&raquo;</span>
+            </span>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+
   return (
-    <Table>
-      <Table.Head>
-        <Table.TextHeaderCell></Table.TextHeaderCell>
-        <Table.TextHeaderCell>Name</Table.TextHeaderCell>
-        <Table.TextHeaderCell>Username</Table.TextHeaderCell>
-      </Table.Head>
-      <Table.Body height={240}>
-        {users.map((user) => (
-          <Table.Row key={user.username}>
-            <Table.Cell display="flex" alignItems="center">
-              <Avatar
-                className={"mr-3"}
-                src={user.icon}
-                name={user.username}
-                size={40}
-              />
-              <Text marginLeft={8} size={300} fontWeight={500}>
-                {user.username}
-              </Text>
-            </Table.Cell>
-            <Table.TextCell>{user.name}</Table.TextCell>
-            <Table.TextCell>{user.username}</Table.TextCell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+    <>
+      <Table className="mt-3">
+        <Table.Head>
+          <Table.TextHeaderCell></Table.TextHeaderCell>
+          <Table.TextHeaderCell>Name</Table.TextHeaderCell>
+          <Table.TextHeaderCell>Username</Table.TextHeaderCell>
+        </Table.Head>
+        <Table.Body height={240}>
+          {users.map((user) => (
+            <Table.Row key={user.username}>
+              <Table.Cell display="flex" alignItems="center">
+                <Avatar
+                  className={"mr-3"}
+                  src={user.icon}
+                  name={user.username}
+                  size={40}
+                />
+                <Text marginLeft={8} size={300} fontWeight={500}>
+                  {user.username}
+                </Text>
+              </Table.Cell>
+              <Table.TextCell>{user.name}</Table.TextCell>
+              <Table.TextCell>{user.username}</Table.TextCell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+      {PagingComponent()}
+    </>
   );
 };
 
